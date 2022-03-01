49cf9b16d1b7bb3595286a700541a2ad5a47ac7b
BBalintB <bbalintb@gmail.com>
2022-03-01 17:58:54 +0100

    Basic logic

--- Superhero/Logic/SuperHeroLogic.cs
+++ Superhero/Logic/SuperHeroLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Mvvm.Messaging;
+using Superhero.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,8 @@ namespace Superhero.Logic
 {
     public class SuperHeroLogic
     {
+        IList<SuperHeroMember> headQuarter;
+        IList<SuperHeroMember> battleground;
+        IMessenger Messenger;
     }
 }
