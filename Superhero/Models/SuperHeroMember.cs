342e49aa67acb1aea6afb3409cb8da4d6823574b
BBalintB <bbalintb@gmail.com>
2022-03-01 17:04:31 +0100

    SuperHeroMember added

--- Superhero/Models/SuperHeroMember.cs
+++ Superhero/Models/SuperHeroMember.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,54 @@ using System.Threading.Tasks;
 
 namespace Superhero.Models
 {
-    public class SuperHeroMember
+    public enum Types { 
+        good,bad,neutral
+    }
+
+    public class SuperHeroMember:ObservableObject
     {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { SetProperty(ref name,value); }
+        }
+
+        private int power;
+
+        public int Power
+        {
+            get { return power; }
+            set { SetProperty(ref power, value); }
+        }
+
+        private int speed;
+
+        public int Speed
+        {
+            get { return speed; }
+            set { SetProperty(ref speed, value); }
+        }
+
+        private Types types;
+
+        public Types Types
+        {
+            get { return types; }
+            set { SetProperty(ref types, value); }
+        }
+
+        public SuperHeroMember GetCopy() {
+            return new SuperHeroMember()
+            {
+                Name = this.Name,
+                Power = this.Power,
+                Speed = this.Speed,
+                Types = this.Types
+            };
+        }
+
+
     }
 }
