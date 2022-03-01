ace0ac9f5fae0bd524db5af6a623cdcef968683a
hallgato <hallgato@LABOR215-17>
2022-03-01 16:41:15 +0100

    Add project files.

---
+++ Superhero/SuperHeroCreatorWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Superhero
+{
+    /// <summary>
+    /// Interaction logic for SuperHeroCreatorWindow.xaml
+    /// </summary>
+    public partial class SuperHeroCreatorWindow : Window
+    {
+        public SuperHeroCreatorWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
