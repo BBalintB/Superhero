4b1ae9ff4acb02b74e8a0c665b2c80ba523a9a41
hallgato <hallgato@LABOR215-18>
2022-03-01 17:54:40 +0100

    EnumToColor, CreatorWindow done(?)

--- Superhero/Logic/SuperHeroLogic.cs
+++ Superhero/Logic/SuperHeroLogic.cs
@@ -8,5 +8,6 @@ namespace Superhero.Logic
 {
     public class SuperHeroLogic
     {
+
     }
 }
---
+++ Superhero/Services/ISuperHeroCreate.cs
@@ -0,0 +1,14 @@
+﻿using Superhero.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superhero.Services
+{
+    public interface ISuperHeroCreate
+    {
+        SuperHeroMember Create();
+    }
+}
