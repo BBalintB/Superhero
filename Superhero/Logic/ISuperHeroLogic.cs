0b7df3057cb07d60a487de72be83e28eac4d18fc
Bárdosi_Bálint <bbalintb@stud.uni-obuda.hu>
2022-03-02 16:48:27 +0100

    SuperHeroLogic and SuperHeroLogic added and created

---
+++ Superhero/Logic/ISuperHeroLogic.cs
@@ -0,0 +1,17 @@
+﻿using Superhero.Models;
+using System.Collections.Generic;
+
+namespace Superhero.Logic
+{
+    public interface ISuperHeroLogic
+    {
+        double AVGPower { get; }
+        double AVGSpeed { get; }
+
+        void AddToBattleground(SuperHeroMember member);
+        void CreateHero();
+        void RemoveFromBattleground(SuperHeroMember member);
+        void SetupCollections(IList<SuperHeroMember> hQ, IList<SuperHeroMember> bF);
+        void SaveHeros();
+    }
+}
--- Superhero/Logic/SuperHeroLogic.cs
+++ Superhero/Logic/SuperHeroLogic.cs
@@ -1,17 +1,76 @@
 ﻿using Microsoft.Toolkit.Mvvm.Messaging;
+using Newtonsoft.Json;
 using Superhero.Models;
+using Superhero.Services;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Superhero.Logic
 {
-    public class SuperHeroLogic
+    public class SuperHeroLogic : ISuperHeroLogic
     {
         IList<SuperHeroMember> headQuarter;
         IList<SuperHeroMember> battleground;
         IMessenger Messenger;
+        ISuperHeroCreate heroCreateService;
+        public SuperHeroLogic(IMessenger messenger, ISuperHeroCreate superHeroCreate)
+        {
+            this.Messenger = messenger;
+            this.heroCreateService = superHeroCreate;
+        }
+        public void SetupCollections(IList<SuperHeroMember> hQ, IList<SuperHeroMember> bF)
+        {
+            this.headQuarter = hQ;
+            this.battleground = bF;
+        }
+        public double AVGPower
+        {
+            get
+            {
+                return Math.Round(battleground.Count == 0 ? 0 : battleground.Average(x => x.Power), 2);
+            }
+        }
+
+        public double AVGSpeed
+        {
+            get
+            {
+                return Math.Round(battleground.Count == 0 ? 0 : battleground.Average(x => x.Speed), 2);
+            }
+        }
+
+        public void AddToBattleground(SuperHeroMember member)
+        {
+            battleground.Add(member);
+            Messenger.Send("Hero added ", "Hero info");
+        }
+
+        public void RemoveFromBattleground(SuperHeroMember member)
+        {
+            battleground.Remove(member);
+            Messenger.Send("Hero removed", "Hero info");
+        }
+
+        public void CreateHero()
+        {
+            SuperHeroMember sH = heroCreateService.Create();
+            if (sH!=null)
+            {
+                headQuarter.Add(sH);
+            }
+            
+            
+        }
+
+        public void SaveHeros() {
+            string jsonDataHQ = JsonConvert.SerializeObject(headQuarter);
+            File.WriteAllText("HQ.json", jsonDataHQ);
+            string jsonDataBattleground = JsonConvert.SerializeObject(battleground);
+            File.WriteAllText("Battleground.json", jsonDataBattleground);
+        }
     }
 }
